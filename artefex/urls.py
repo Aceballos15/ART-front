ec1237a3349c828946faf9580cdfa37dc53eeac3
Aceballos15 <ceballoscardonaalexander@gmail.com>
2023-01-27 16:56:57 -0500

    Second commit

--- artefex/urls.py
+++ artefex/urls.py
@@ -22,6 +22,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('shop/', views.shop, name='shop')
     
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- artefex/views.py
+++ artefex/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home/index.html")
+
+def shop(request): 
+    return render(request, "home/shop.html") 
