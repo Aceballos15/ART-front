f85ceddf21516e4c24a30e4e9ba56ac2661fe282
Aceballos15 <ceballoscardonaalexander@gmail.com>
2023-01-31 19:52:34 -0500

    Add home

--- artefex/views.py
+++ artefex/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home/index.html")
+    lista= [0,1 ,2 ,3, 4, 5 ]
+    return render(request, "home/index.html", {'lista': lista})
 
 def shop(request): 
     return render(request, "home/shop.html") 
